a5c0e9353c772b2ceede7f52caf3fd8865815c8f
Jackaboy50 <119588287+Jackaboy50@users.noreply.github.com>
2023-10-15 14:33:19 +0100

    changed file structure

---
+++ Minesweeper Project/Tile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Minesweeper_Project
+{
+    /// <summary>
+    /// Represents a tile within the board 
+    /// </summary>
+    class Tile
+    {
+        public string name;
+        public bool ismine;
+        public int xcoord;
+        public int ycoord;
+        /// <summary>
+        /// Initializes a new instance of the Tile class
+        /// </summary>
+        /// <param name="x">Specifies the X position of the tile</param>
+        /// <param name="y">Specifies the Y position of the tile</param>
+        /// <param name="n">Specifies the name of the tile</param>
+        /// <param name="i">Specifies whether the tile is a mine</param>
+        public Tile(int x, int y, string n, bool i)
+        {
+            name = n;
+            xcoord = x;
+            ycoord = y;
+            ismine = i;
+        }
+    }
+}
